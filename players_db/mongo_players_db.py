4abbc93ae5454592c1e20103eceb65412c6ca48a
Jeremy <jeremy.wambecke@univ-grenoble-alpes.fr>
2024-01-19 16:14:43 +0000

    Add read methods for mongo

--- players_db/mongo_players_db.py
+++ players_db/mongo_players_db.py
@@ -1,7 +1,8 @@
 import bson
 import pymongo
 from bson.objectid import ObjectId
-from typing import Any, Dict
+from typing import Any, Dict, Union
+from collections.abc import Iterable
 from .players_db import PlayersDB
 
 
@@ -30,11 +31,31 @@ class MongoPlayersDB(PlayersDB):
         inserted = collection.insert_one(item)
         return inserted.inserted_id
 
-    def get_leagues_names(self):
-        return [league["name"] for league in self.leagues_collection.find()]
+    @staticmethod
+    def get_item(collection, item_id: Union[ObjectId, str]):
+        if isinstance(item_id, str):
+            item_id = ObjectId(item_id)
+        return next(collection.find({"_id": item_id}))
 
-    def get_league(self, league_id: str):
-        return self.leagues_collection.find({"id": league_id})
+    def get_leagues(self):
+        return list(self.leagues_collection.find())
 
-    def get_player(self, player_id: str):
-        return next(self.players_collection.find({"_id": ObjectId(player_id)}))
+    def get_league(self, league_id: Union[ObjectId, str]):
+        return self.get_item(self.leagues_collection, league_id)
+
+    def get_team(self, team_id: Union[ObjectId, str]):
+        return self.get_item(self.teams_collection, team_id)
+
+    def get_player(self, player_id: Union[ObjectId, str]):
+        return self.get_item(self.players_collection, player_id)
+
+    @staticmethod
+    def replace_object_ids(data: Any):
+        if isinstance(data, Dict):
+            for key, value in data.items():
+                data[key] = MongoPlayersDB.replace_object_ids(value)
+        elif isinstance(data, Iterable) and not isinstance(data, str):
+            data = [MongoPlayersDB.replace_object_ids(value) for value in data]
+        elif isinstance(data, ObjectId):
+            return str(data)
+        return data
